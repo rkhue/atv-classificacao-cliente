45ded892313b9177ab8401b306f8f5d277d0adac
rkhue <85773727+rkhue@users.noreply.github.com>
2026-05-14 10:43:16 -0300

    refactor: update DataFrame variable usage and add adjusted score calculation

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ CSV_PATH = CURRENT_DIR / 'assets' / 'credito_banco.csv'
 # -----------------------------------------------------------------------
 
 # 2.1 Leitura
-df_credito = pd.read_csv(CSV_PATH)
+df_gerado = pd.read_csv(CSV_PATH)
 
 # 2.3 Classe de colunas (base em credito_banco.csv)
 class Col:
@@ -30,16 +30,18 @@ class Col:
     SCORE_EXTERNO = 'score_externo'
     INADIMPLENTE = 'inadimplente'
 
+    # Score ajustado
+    SCORE_AJUSTADO = 'score_ajustado'
 
 # 3. Limpeza e pré-processamento
 # -----------------------------------------------------------------------
 
 ## 3.1 Limpeza de duplicados
-df = df_credito.drop_duplicates()
+df_credito = df_gerado.drop_duplicates()
 
 ## 3.2 Limpeza de valores nulos (preencher com a média da coluna)
 for column in [Col.IDADE, Col.RENDA_ANUAL, Col.DIVIDA_ATUAL, Col.HISTORICO_PAGAMENTO, Col.SCORE_EXTERNO]:
-    df[column].fillna(df[column].mean(), inplace=True)
+    df_credito[column].fillna(df_credito[column].mean(), inplace=True)
 
 ## 3.3 Remover outliers (baseado no IQR - Interquartile Range)
 def remove_outliers(df, column_name):
@@ -52,18 +54,16 @@ def remove_outliers(df, column_name):
     return df
 
 ## Somente idade e renda
-df = remove_outliers(df, Col.IDADE)
-df = remove_outliers(df, Col.RENDA_ANUAL) 
+df_credito = remove_outliers(df_credito, Col.IDADE)
+df_credito = remove_outliers(df_credito, Col.RENDA_ANUAL) 
 
-# 4. Cálculo Score Ajustado
-# -----------------------------------------------------------------------
 
-# Colunas para o cálculo do score ajustado
-renda = df[Col.RENDA_ANUAL].values
-idade = df[Col.IDADE].values
-score_externo = df[Col.SCORE_EXTERNO].values
+## 3.4 Colunas para o cálculo do score ajustado
+renda = df_credito[Col.RENDA_ANUAL].values
+idade = df_credito[Col.IDADE].values
+score_externo = df_credito[Col.SCORE_EXTERNO].values
 
-# Definir Fatores Renda
+## 3.5 Definir Fatores de Renda e Idade para ajuste do score
 
 # Escolhemos quando a renda for maior que 150.000 recebe 1.2 a mais no score
 # Quando a renda estiver entre 50.000 e 150.000 mantém o score
@@ -77,9 +77,13 @@ fator_idade = np.where(idade > 50, 1.2, np.where(idade >= 25, 1.0, 0.8))
 
 score_ajustado = score_externo * fator_renda * fator_idade
 
-df['score_ajustado'] = score_ajustado
+## 3.6 Adicionar a coluna de score ajustado ao DataFrame
+df_credito[Col.SCORE_AJUSTADO] = score_ajustado
 
-df.to_csv(CSV_PATH, index=False)
+
+# 4. Machine Learning - Classificação
+# -----------------------------------------------------------------------
+
 
 # 8. Montagem de gráficos com matplotlib
 # -----------------------------------------------------------------------
@@ -92,13 +96,13 @@ inadimplentes = df_credito[df_credito[Col.INADIMPLENTE] == 1]
 plt.figure(figsize=(10, 4))
 
 plt.subplot(1, 2, 1)
-plt.hist(adimplentes.renda_anual, color='purple', edgecolor='white')
+plt.hist(adimplentes[Col.RENDA_ANUAL], color='purple', edgecolor='white')
 plt.title("Distribuição de Renda de Adimplentes")
 plt.xlabel("Renda (R$)")
 plt.ylabel("Frequência")
 
 plt.subplot(1, 2, 2)
-plt.hist(inadimplentes.renda_anual, color='orange', edgecolor='white')
+plt.hist(inadimplentes[Col.RENDA_ANUAL], color='orange', edgecolor='white')
 plt.title("Distribuição de Renda de Inadimplentes")
 plt.xlabel("Renda (R$)")
 plt.ylabel("Frequência")
